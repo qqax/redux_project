e1db1c86d55f002a3d09b72caee5e078a1d5b134
Alex <alar0@yahoo.com>
2021-02-04 14:36:06 +0300

    add app.js, Connect, mapStateToProps, mapDispatchToProps to counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {connect} from "react-redux";
+import {inc, dec, rnd} from "../actions";
 import PropTypes from "prop-types";
 
 const Counter = ({counter, inc, dec, rnd}) => {
@@ -19,4 +21,21 @@ Counter.propTypes = {
     rnd: PropTypes.func
 };
 
-export default Counter;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        inc: () => dispatch(inc()),
+        dec: () => dispatch(dec()),
+        rnd: () => {
+            const value = Math.floor(Math.random() * (Math.random() > 0.5 ? 10 : -10));
+            dispatch(rnd(value));
+        },
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
